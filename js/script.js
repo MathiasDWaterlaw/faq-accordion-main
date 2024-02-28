229ee0ab72c40bcd48f39b5c53ddce349bf57046
MathiasDWaterlaw <mathiasdellosa@gmail.com>
2024-02-28 04:19:53 +0100

    added animation

--- js/script.js
+++ js/script.js
@@ -1,20 +1,13 @@
 const questionTitle = document.querySelectorAll('.question-title');
 const answers = document.querySelectorAll('.answer');
 
-// const openAnswer = (element) => {
-//     element.querySelector('.plus').classList.toggle('hidden');
-//     element.querySelector('.minus').classList.toggle('hidden');
-
-// }
-
-questionTitle.forEach((element, index) => {
+questionTitle.forEach((element) => {
     element.addEventListener('click', () => {
         element.querySelector('.plus').classList.toggle('hidden');
         element.querySelector('.minus').classList.toggle('hidden');
         answers.forEach((answer) => {
             if (element.querySelector('p').id === answer.id) {
-                answer.classList.toggle('hidden');
-                answer.style.transition = '0.5s';
+                answer.classList.toggle('active');
             }
         });
     });
